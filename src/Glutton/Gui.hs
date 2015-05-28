4c89c3f52a0eb7cbb81d64be296c6bfd90908140
Alan Cocanour <alancocanour@gmail.com>
2015-05-27 20:02:49 -0700

    Add delete subscriptions button

--- src/Glutton/Gui.hs
+++ src/Glutton/Gui.hs
@@ -41,12 +41,12 @@ sumUnreadCount = sum . map (\(_,c,_)->c)
 
 -- | Constructs the GUI
 setup :: Behavior [UnreadCount] -> Updater -> Window -> UI ()
-setup ucs updater window = do --TODO use the Updater to enable adding/removing subscriptions from the GUI
+setup ucs updater window = do
   _ <- sink title (fmap ((++ " - Glutton") . show . sumUnreadCount) ucs) (return window)
 
   activeFeed <- div # set id_ "activeFeed"
 
-  let sidebarContents = fmap (map $ mkSidebar activeFeed) ucs
+  let sidebarContents = fmap (map $ mkSidebar updater activeFeed) ucs
   sidebar <- div
              # set id_ "sidebar"
   sinkChildren sidebarContents (element sidebar)
@@ -63,22 +63,25 @@ sinkChildren b e = do
     elements' <- sequence elements
     e # set children elements'
 
-mkSidebar :: Element -> UnreadCount -> UI Element
-mkSidebar activeFeed (n, c, h) = do
+mkSidebar :: Updater -> Element -> UnreadCount -> UI Element
+mkSidebar updater activeFeed (n, c, h) = do
   let countString = if c > 0 then show c else ""
   feedSelector <- div #. "feedSelector"
                   #+ [ span # set text n
                      , span # set text countString #. "unreadCount" ]
-  on click feedSelector $ const $ mkActiveFeed activeFeed h
+  on click feedSelector $ const $ mkActiveFeed updater activeFeed h
   return feedSelector
 
-mkActiveFeed :: Element -> SubscriptionHandle -> UI ()
-mkActiveFeed activeFeed handle = do
+mkActiveFeed :: Updater -> Element -> SubscriptionHandle -> UI ()
+mkActiveFeed updater activeFeed handle = do
   sub <- liftIO $ S.get handle
   markAllReadButton <- span #. "button" # set text "♺ Mark All Read"
+  deleteButton <- span #. "button" # set text "☢ Delete"
   header <- div #+
             [ span #. "activeFeedTitle" # set text (feedTitle sub)
-            , element markAllReadButton ]
+            , element markAllReadButton
+            , element deleteButton
+            ]
   _ <- element activeFeed # set children []
   _ <- element activeFeed #+ (element header
                               : map (mkItem handle) (feedItems sub) )
@@ -87,10 +90,19 @@ mkActiveFeed activeFeed handle = do
     liftIO $ modify handle $ \s ->
       let items' = map (\i -> i {itemRead = True}) $ feedItems s
       in s { feedItems = items'}
-    mkActiveFeed activeFeed handle
+    mkActiveFeed updater activeFeed handle
+
+  on click deleteButton $ const $ do
+    liftIO $ feedUrl <$> S.get handle >>= removeSubscription updater
+    element activeFeed # set children []
 
 mkItem :: SubscriptionHandle -> ItemState -> UI Element
 mkItem sh i = do
+  link <- a # set (attr "target") "_blank"
+            # set (attr "href") (fromMaybe "" $ itemLink i)
+            # set text (fromMaybe "" $ itemTitle i)
+            #. if itemRead i then "read" else "unread"
+
   markRead <- span #. "clickable" # set text (check $ itemRead i)
   on click markRead $ const $ do
     newReadState <- liftIO $ modifyAndRead sh $ \s ->
@@ -100,12 +112,9 @@ mkItem sh i = do
               in (item { itemRead = newReadState }, First $ Just newReadState)
             else (item, First Nothing)
       in (s { feedItems = map fst items' }, getFirst $ mconcat $ map snd items')
-    element markRead # set text (check $ fromMaybe True newReadState)
+    _ <- element markRead # set text (check $ fromMaybe True newReadState)
+    element link # set class_ (if fromMaybe True newReadState then "read" else "unread")
   
-  link <- a # set (attr "target") "_blank"
-            # set (attr "href") (fromMaybe "" $ itemLink i)
-            # set text (fromMaybe "" $ itemTitle i)
-            #. if itemRead i then "read" else "unread"
   div #. "feedItem"
     #+ [ element markRead
        , element link ]
